71417ae9b7ea380424a65765ce4fdceb8d0b6ca6
TaehyeungKim <kth1837@snu.ac.kr>
2022-07-31 00:07:03 +0900

    :art: Modifying the swipe button of feed

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -150,7 +150,8 @@ function Feed({feedData, profileImageData}: FeedProps) {
                     <div className = {styles.buttonArea} id ={styles.left}>
                         <button onClick = {()=>switchShownPhotos('left')}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
+                                <path d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
+                                <circle r="8" cx="8" cy="8"></circle>
                             </svg>
                         </button>
                     </div> : null}
@@ -158,7 +159,8 @@ function Feed({feedData, profileImageData}: FeedProps) {
                     <div className = {styles.buttonArea} id = {styles.right}>
                         <button onClick = {()=>switchShownPhotos('right')}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
+                                <path d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
+                                <circle r="8" cx="8" cy="8"></circle>
                             </svg>
                 </button></div> : null}
                     <div className = {styles.wrapper} ref={frame} id="frame">
